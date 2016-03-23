9339bfa90650d5936c0a288e68c6dd6d7b2414a1
Kimberly Oleiro <koleiro@gmail.com>
2016-03-23 16:33:27 -0400

    final submission

--- exercise6.rb
+++ exercise6.rb
@@ -20,6 +20,26 @@ puts grocery_list.count
 
 puts ".........."
 
-puts grocery_list.include?("rice")
+if grocery_list.include?("bananas")
+  puts "You don't need to pick up bananas today"
+else
+  puts "You need to pick up bananas"
+end
+
+puts "........."
 
 puts grocery_list[1]
+
+puts "........."
+
+grocery_list << "bananas"
+
+print_grocery_list(grocery_list.sort)
+
+puts "........."
+
+puts "I cant find hummus anywhere. Here's an updated list:"
+
+grocery_list.delete("hummus")
+
+print_grocery_list(grocery_list.sort)
