214d2cb9512872549de199296b8e6c15aa28546d
Kimberly Oleiro <koleiro@gmail.com>
2016-03-23 17:20:32 -0400

    Increasing cohort size

--- exercise7.rb
+++ exercise7.rb
@@ -7,3 +7,23 @@ students = {
 students.each do |cohort, number_students|
   puts "#{cohort}: #{number_students} students"
 end
+
+puts ".........."
+
+students[:cohort4] = 43
+
+students.each do |cohort, number_students|
+  puts "#{cohort}: #{number_students} students"
+end
+
+puts ".........."
+#using the key method to print out the cohort names
+
+puts students.keys
+
+puts ".........."
+
+#expand each cohort size by 5%
+students.each do |cohort, number_students|
+  puts "#{cohort}: #{number_students*1.05} students"
+end
