268e92192fab95ed69eb21dbde5b10a1e99c7ba9
Kimberly Oleiro <koleiro@gmail.com>
2016-03-23 14:12:35 -0400

    exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,14 @@
+puts "What's the temperature today (in Fahrenheit)?"
+temperature_fahrenheit = gets.chomp
+
+puts "So its #{temperature_fahrenheit} degrees fahrenheit"
+puts "hmm I wonder what that would be in celsius"
+
+def f_to_celsius(temperature_fahrenheit)
+  (temperature_fahrenheit.to_i - 32) * 5/9
+
+end
+
+temperature_celsius = f_to_celsius(temperature_fahrenheit)
+
+puts "if my calculations are correct that would be #{temperature_celsius}"
